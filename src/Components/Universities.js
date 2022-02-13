885290def0d9c73478b61128f244ddb18c8ee40e
Harsh Patil <harshnilkanth2015@gmail.com>
2022-02-13 13:51:55 -0700

    v3.0 University Site Completed
    - University site has a dropdown with countries fetched from an API. After selecting the country, the universities in the country appear.

---
+++ src/Components/Universities.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import '../CSS/Universities.css';
+
+const URL_UNI_INFO = "http://universities.hipolabs.com/";
+const URL_COUNTRIES_INFO = "https://countriesnow.space/api/v0.1/countries/info?returns=none"
+
+
+function Dropdown(props) {
+    const options = props.options;
+    
+    function handleSelection(e) {
+        props.assignSelection(e.target.innerHTML);
+    }
+    const mappedOptions = options.map((ele, index)=>{
+        return <button onClick={handleSelection} className='dropdown-elements' key={index}>{ele.name}</button>
+    });
+
+    return(
+        <div>
+        <ul className='dropdown'>
+            {mappedOptions}
+        </ul>
+
+        </div>
+    )
+}
+
+function UniList(props) {
+    const mappedUnis = props.options.map((ele, index)=>{
+        return <li className='uni-element' key={index}>{ele.name}</li>
+    })
+
+    return(
+            <div>
+            {props.update()}
+                {mappedUnis}
+            </div>
+        )
+}
+
+export default function Universities() {
+
+    const [countries, setCountries] = useState([]);
+    const [universities, setUniversities] = useState([]);
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+    const [selection, setSelection] = useState('Canada');
+
+    function assignSelection(s) {
+        setSelection(s);
+    }
+
+    function update() {
+        fetch(URL_UNI_INFO + 'search?country=' + selection).then(response=>response.json())
+        .then((json)=>setUniversities(json));
+    }
+
+    useEffect(()=> {
+        fetch(URL_COUNTRIES_INFO).then(response=>response.json())
+        .then((json)=>json["data"])
+        .then(res=>{setCountries(res)})
+    },[])
+
+
+    return (
+        <div>
+            <button onClick={()=> setMenuIsOpen(!menuIsOpen)}className='selection-button'>+ Select a Country</button>
+            {menuIsOpen && <Dropdown options={countries} assignSelection={assignSelection}/>}
+            <h2> Universities in {selection}</h2>
+            <UniList options={universities} update={update}/>
+        </div>
+    )
+}
